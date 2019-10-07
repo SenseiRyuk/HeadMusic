ad0ca11c684fcb208b886a429603730f44736d2b
Florian <florianbucheron@gmail.com>
2019-10-07 17:00:51 +0200

    Modifs contraintes et Action bar

--- app/src/main/java/com/example/if26new/MainActivity.java
+++ app/src/main/java/com/example/if26new/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -50,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         final ControlerLayouts controler = ControlerLayouts.getInstance();
